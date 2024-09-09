ee09b69e2fab1454f4b16ef950258a100a0ddb3d
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-09 20:42:07 +0000

    added variables

--- variables/practice.py
+++ variables/practice.py
@@ -1,1 +1,12 @@
-name = ("hello")
+name = ("sebastian")
+lilNum = 4
+bigNum = 67
+breakfast = ("43-bananas")
+favColor = ("RoyalPurple")
+schoolname = ("UCAS")
+Year = 2024
+eyecolor = ("brown")
+myage = 15
+favSubject = ("Lunch!")
+
+print (name,lilNum,bigNum,breakfast,favColor,schoolname,Year,eyecolor,myage,favSubject)
