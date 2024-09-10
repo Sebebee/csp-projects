0bf1b552fb19094f323f1bc793aa812988068fd3
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-10 20:11:26 +0000

    finished assignment

--- variables/practice.py
+++ variables/practice.py
@@ -1,12 +1,12 @@
 name = ("sebastian")
 lilNum = 4
 bigNum = 67
-breakfast = ("43-bananas")
-favColor = ("RoyalPurple")
-schoolname = ("UCAS")
+breakfast =  ("43-bananas")
+favColor =  ("RoyalPurple")
+schoolname =  ("UCAS")
 Year = 2024
 eyecolor = ("brown")
 myage = 15
-favSubject = ("Lunch!")
+favSubject = ("Lunch")
 
 print (name,lilNum,bigNum,breakfast,favColor,schoolname,Year,eyecolor,myage,favSubject)
