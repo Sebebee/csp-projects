dc981a6ce42ccaba10be85ebd1c5c62feb15bead
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-10 20:41:37 +0000

    started work

---
+++ variables/practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World:");
+    char myname[]="sebastian";
+    int lilnum =5;
+    int bignum=194;
+    char myBreakfast[]="shake";
+    char myfavcolor[]="RoyalPurple";
+    char schoolName[]="Utah County Academy of sciences";
+    int year=2024;
+    char eyecolor[]="notblue";
+    int myage=15;
+    char favsubject[]="lunch";
+printf(myname,lilnum,bignum,myBreakfast,myfavcolor,schoolName,year,eyecolor,myage,favsubject);
+    return 0;
+   
+}
