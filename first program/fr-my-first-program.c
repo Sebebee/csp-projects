bc09bbd4530770fda575e2cd2cc3842ecfb17a00
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-16 20:32:30 +0000

    can i get a number 2 number1

---
+++ first program/fr-my-first-program.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+   char order[43];
+printf("welcome to freshavacdos what will your order be for today??:\n");
+scanf("%s", order);
+printf("what do you want me to do with a %s", order);
+    return 0;
+   
+}
