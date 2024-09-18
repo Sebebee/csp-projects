0ffbe419cbd1e6b46bcef45343616aad975c7ac8
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-18 20:41:42 +0000

    i almost done

--- expressions/Ep.py
+++ expressions/Ep.py
@@ -7,4 +7,53 @@ num5 = 6
 
 print(num1-num2/num3*num4+num5) #just repeat this algorithim and update the variables
 
+num1 = 18
+num2 = 3
+num3= 7
+num4 = 2
+num5 = 5
 
+print(num1/num2-num3+num4*num5)
+
+num1=6
+num2=4
+num3=12
+num4=72
+num5=8
+num6=9
+
+print(num1*num2/num3+num4/num5-num6)
+
+num1=17
+num2=6
+num3=2
+num4=4
+num5=3
+
+
+print((num1-num2/num3)+num4*num5)
+
+num1= -2*1
+num22= -2*4
+num2= -2*-2
+num3= -2*2
+num4= 6*2-3
+answer= num1+num22+num2+num3+num4
+print(answer)
+
+num1= 3-4*7
+num22= num1/5
+num2= -1*num22
+num3= -2*24/6
+answer= num2+num3
+print(answer)
+
+equation1=3*5**2/15
+equation2=-5+2**2
+answer=equation1+equation2
+print(answer)
+
+equation1=1**4*2**2+3**3
+equation2=-2**5/4
+answer=equation1+equation2
+print(answer)
