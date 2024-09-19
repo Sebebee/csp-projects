da218318617ca8df9b86a45a717b42dcc8af037e
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-19 03:07:45 +0000

    my eyes hurt

---
+++ expressions/expresionpractice.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int num = pow(2,5);
+int num1= 1, num2=2, num3=3,num4=4,num5=5,num6=6,num7=7,num8=8,num9=9,num12=12,num18=18,num15=15,num22=22,num24=24,num72=72;
+int equation1 = num7-num24/num8*num4+num6;
+printf("%d \n", equation1 );
+   
+   int equation2 =num18/num3-num7+num2*num5;
+   printf("%d \n",equation2);
+
+   int equation3=num6*num4/num12+num72/num8-num9;
+   printf("%d \n",equation3);
+
+int equation4 = 17-num6/num2;
+int equation4AndaHalf = num4*num3;
+int answer = equation4+equation4AndaHalf;
+
+printf("%d \n",answer);
+
+int part1 = -2*num1+-2*num4+-2*-2+-2/num2;
+int part2 = num6+num2-num3;
+
+int answer1 = part1+part2;
+
+printf("%d \n",answer1);
+
+int part = -1*(num3-num4*num7);
+int part3 = part/5;
+int part4 = num2*num24/num6;
+int final = part3 - part4;
+
+
+printf("%d \n",final);
+
+int equation5 = 3* pow(5,2) / num15;
+int equation6 = (num5-pow(2,2));
+int final2 = equation5 - equation6;
+
+printf("%d \n",final2);
+
+int eEquation = pow(1,4) * pow(2,2) + pow(3,3);
+int eEquation1 = pow(-2,5) / num4;
+int final3 = eEquation + eEquation1;
+printf("%d \n",final3);
+
+int eq1 =pow(22/2-2*5,2);
+int eq2 = pow(4-6/6,2);
+int finalAnswer = eq1+eq2;
+printf("%d \n",finalAnswer);
+    return 0;
+}
