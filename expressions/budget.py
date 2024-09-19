12d7bddf62767fa6bfcb1b49558059f3fafdd200
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-19 20:42:07 +0000

    music

---
+++ expressions/budget.py
@@ -0,0 +1,8 @@
+rent = ""
+utilities = ""
+Groceries = ""
+transportation = ""
+
+print("This low level calculator made by some guy who needs a good grade will kinda calculate your expenses.")
+input()
+
