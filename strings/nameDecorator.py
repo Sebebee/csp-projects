857de9f5af520f2fb85853ae2e118ae54050e10c
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-26 20:07:05 +0000

    88 ate 8

--- strings/nameDecorator.py
+++ strings/nameDecorator.py
@@ -1,4 +1,15 @@
 name = input("what is your name?:")
-
+#((( Name )))
+#--- Name ---
+#:) :) Name :) :)
+### Name ###
+##+++ Name +++
+#~~~ Name ~~~
+#=== Name ===
 print("<<<<<",name,">>>>>")
 print("(((",name,")))")
+print("---",name,"---")
+print(":) :)",name,":) :)")
+print("+++",name,"+++")
+print("~~~",name,"~~~")
+print("===",name,"===")
