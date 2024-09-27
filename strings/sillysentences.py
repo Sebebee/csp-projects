c75d26e54bf7cc601da1645591e13f7ba43fc0aa
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-27 20:41:26 +0000

    wo

---
+++ strings/sillysentences.py
@@ -0,0 +1,9 @@
+#low budget madlib maker
+
+name= input("type a name:")
+place= input("type a place:")
+verb = input("type a verb:")
+noun = input("type a noun:")
+
+name = print(name,"went to the") 
+
