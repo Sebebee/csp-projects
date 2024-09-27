ca736451aea32585b9ace3bfd127ab003c6ccbb2
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-27 20:26:58 +0000

    put it on the beat bro

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 int main(void){
-   char name[]="sebebee";
-   char prefix[]="(((";
+   char name[20];
+   printf("what is your name???:");
+   scanf("%s",name);
+   char prefix[50]="(((";
    char afterfix[]=")))";
    strcat(prefix,name);
-   printf("%s",prefix);
-  strcat(prefix,afterfix);
-   printf("%s\n",prefix);
+ strcat(prefix,afterfix);
+  printf("%s\n",prefix);
+
+
 
     return 0;
    
---
+++ strings/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+s",prefix);
+ strcat(prefix
