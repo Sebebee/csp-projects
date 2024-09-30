feffb86f1acd514cd53e90b1dabbcab8a7af6655
Sebebee <sebastian.miranda@ucas-edu.net>
2024-09-30 01:42:29 +0000

    this is a text message

--- strings/nameDecorator.py
+++ strings/nameDecorator.py
@@ -1,4 +1,5 @@
 name = input("what is your name?:")
 prefix = input("what would you like your prefix to be?:")
 afterfix = input("what would you like your afterfix to be?:")
-print(prefix+name+afterfix)
+sentence = prefix+name+afterfix
+print(sentence)
