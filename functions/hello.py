4275f9735a197c6d8812e9cc32880c537cdbe592
Sebebee <sebastian.miranda@ucas-edu.net>
2024-10-03 03:25:40 +0000

    function no function

--- functions/hello.py
+++ functions/hello.py
@@ -1,12 +1,10 @@
-
-def hello():
-    name = input("what is your name?: ")
+def hello(name):
     greetings = " hello "  
     sentence = greetings + name
     print(sentence)
 
-hello()
-hello()
-hello()
-hello()
-hello()
+hello("john")
+hello("King richard the 3rd")
+hello("Archie")
+hello("Queen Elizabeth the 2nd")
+hello("Joseph stalin")
