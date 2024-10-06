7c5152d619394eea0e27c277468f2d98385994e5
Sebebee <sebastian.miranda@ucas-edu.net>
2024-10-06 03:21:59 +0000

    roo rraa

--- expressions/budget.py
+++ expressions/budget.py
@@ -1,6 +1,6 @@
 def enlightenMe(type):
-  insert = input(f"what is your monthly {type}: ")
-  return int(insert)
+  enter = input(f"what is your monthly {type}: ")
+  return int(enter)
 
 
 
@@ -10,7 +10,7 @@ rent= enlightenMe("rent")
 utilities= enlightenMe("utilites")
 Groceries= enlightenMe("groceries")
 transportation= enlightenMe("transportation")
-percentageSavings = enlightenMe("what percentage of your income do you wanna save?")
+
 
 def calculate(type,amount):
  per = amount/inIncome * 100
@@ -22,11 +22,14 @@ Groceriesp=Groceries/inIncome
 transportationP=transportation/inIncome
 
 expenses = inIncome-rent-utilities-Groceries-transportation
-savings= inIncome/percentageSavings
+savings= inIncome*.2
 spending = inIncome-expenses
 
-print("Your monthly savings is ",savings*100)
+print("Your monthly savings is ",savings)
 print("Your monthly expenses is ", expenses)
 print("Your spending money is",spending)
  
 calculate("rent",rent)
+calculate("utilities",utilities)
+calculate("groceries",Groceries)
+calculate("transportation",transportation)
