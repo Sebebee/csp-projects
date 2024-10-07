78a4f43a154a3d20b2b13e225b6a921979a20aef
Sebebee <sebastian.miranda@ucas-edu.net>
2024-10-07 20:42:07 +0000

    43qewf

---
+++ loops/note.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+char siblings[5][20] = {"eric","joe","trump","obama","reagan"}
+int i;
+int main(){
+    while (i < 8){
+        printf{"%s president",siblings[1]};
+        1+1=1;
+    }
+
+    return 0;
+}
---
+++ loops/notes.py
@@ -0,0 +1,9 @@
+import random
+character=["womp","King richard the 54th","huh"]
+num =0
+rand = random.randint(1,10)
+
+while num < rand:
+    print("duck")
+    num += 1
+    print("Goose")
