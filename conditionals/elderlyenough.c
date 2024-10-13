487da2742f0d4c678115a9639ca8c077a9fbd586
Sebebee <sebastian.miranda@ucas-edu.net>
2024-10-13 22:31:47 +0000

    sigma?

--- conditionals/elderlyenough.c
+++ conditionals/elderlyenough.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main (void){
- int age[80];
+ int age;
 printf("What is your age?\n:");
 scanf("%d", age);
 if (age>=18){
     printf("You are old enough to vote");
 }else if (age<18 && age>=16){
     printf("You are old enough to drive");
-}else if (age<16 && age==15){
+}else if (age==15){
     printf("You are old enough for a learners permit");
 }else if (age<15 && age>=5){
     printf("You are old enough to go to school");
